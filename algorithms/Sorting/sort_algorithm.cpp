7b3a7fa06a65efdb8cbae35e8cd8bc73398b751f
Backspace <nekitshero@gmail.com>
2021-07-18 15:42:17 +0300

    added selection, quick and insertion sort

--- algorithms/Sorting/sort_algorithm.cpp
+++ algorithms/Sorting/sort_algorithm.cpp
@@ -1,5 +1,6 @@
 #include "sort_algorithm.h"
 #include "../../data-structures/max_heap/max_heap.h"
+#include <iostream>
 
 // Average Time Complexity - O(n * log(n))
 void sort_algorithm::heap_sort(int* arr, const size_t& size)
@@ -30,9 +31,74 @@ void sort_algorithm::bubble_sort(int* arr, const size_t& size)
 	}
 }
 
+// Average Time Complexity - O(n^2)
+void sort_algorithm::insertion_sort(int* arr, const size_t& size)
+{
+	for (size_t i = 1; i < size; i++)
+	{
+		if (arr[i] < arr[i - 1])
+		{
+			int j = i;
+			do 
+			{
+				if (arr[j] < arr[j - 1])
+					swap(arr[j], arr[j - 1]);
+				j--;
+			} while (j >= 0);
+		}
+	}
+}
+
+// Time Complexity - O(n * log(n))
+void sort_algorithm::quick_sort(int* arr, const size_t& low, const size_t& high)
+{
+	if (low < high)
+	{
+		int index = partition(arr, low, high);
+		quick_sort(arr, low, index - 1);
+		quick_sort(arr, index + 1, high);
+	}
+}
+
+// Time Complexity - O(n^2)
+void sort_algorithm::selection_sort(int* arr, const size_t& size)
+{
+	for (size_t i = 0; i < size; i++)
+	{
+		int temp = arr[i], index = i;
+		for (size_t j = i + 1; j < size; j++)
+		{
+			if (arr[j] < temp)
+			{
+				temp = arr[j];
+				index = j;
+			}
+		}
+		swap(arr[i], arr[index]);
+	}
+}
+
 void sort_algorithm::swap(int& a, int& b)
 {
 	auto temp = a;
 	a = b;
 	b = temp;
 }
+
+int sort_algorithm::partition(int* arr, const int& low, const int& high)
+{
+	int pivot = arr[high];
+	int index = low;
+	for (int i = low; i < high; i++)
+	{
+
+		if (arr[i] <= pivot)
+		{
+			swap(arr[i], arr[index]);
+			index++;
+		}
+	}
+	swap(arr[index], arr[high]);
+	return index;
+}
+
--- algorithms/Sorting/sort_algorithm.h
+++ algorithms/Sorting/sort_algorithm.h
@@ -7,9 +7,13 @@ class sort_algorithm
 public:
 	static void heap_sort(int* arr, const size_t& size);
 	static void bubble_sort(int* arr, const size_t& size);
+	static void insertion_sort(int* arr, const size_t& size);
+	static void quick_sort(int* arr, const size_t& low, const size_t& high);
+	static void selection_sort(int* arr, const size_t& size);
 private:
 	sort_algorithm() = delete;
 	static void swap(int& a, int& b);
+	static int partition(int* arr, const int& low, const int& high);
 };
 
 #endif
